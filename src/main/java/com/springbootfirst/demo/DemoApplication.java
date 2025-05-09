c5ac2bd11f020dbca73bd0e1049c09a7c6b4799d
PadmakshTerkar <86471027+PadmakshTerkar@users.noreply.github.com>
2025-05-09 18:04:59 +0530

    Update DemoApplication.java

--- src/main/java/com/springbootfirst/demo/DemoApplication.java
+++ src/main/java/com/springbootfirst/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.springbootfirst.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Springboot Project
 @SpringBootApplication
 public class DemoApplication {
 
